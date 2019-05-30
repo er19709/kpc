c8347acbda76448dc15c8bc2ed5f505678456b70
er19709 <winiaum7@gmail.com>
2019-05-30 16:02:14 +0900

    탭 메뉴 제어를 위한 함수

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let header = $('.header');
 let item = $('.menu-item');
+let tab=$('.tab');
+let panel=$('.notice, .pds');
 
 item.attr('tabindex', '0');
 
@@ -10,4 +12,11 @@ item.on('mouseover focusin', function(){
 
 header.on('mouseout', function(){
   item.removeClass('menu-act');  
+});
+
+tab.on('click keyup', function(e){
+  if(e.type === 'click' || e.keyCode === 13){
+    panel.removeClass('board-act');
+    $(this).parent().addClass('board-act');
+  };
 });
